c156d236a966e3740bd1cccb17566ba635adbe01
root <root@hatsune.thisis.moe>
2016-03-02 20:10:45 +0000

    Improved comments

--- ICS3U/src/mcnellen/HelloWorld.java
+++ ICS3U/src/mcnellen/HelloWorld.java
@@ -5,10 +5,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		/* HelloWorld.java
 		 * This program prints Hello World! to the console
-		 * Quinlan McNellen
+		 * @author Quinlan McNellen
 		 * 2016/03/02
 		 */
 		System.out.println("Hello World!");
+		//End of class
 	}
 
 }
