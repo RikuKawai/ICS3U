af45aef27a8d3513999f89b7bf96e486f2e5aca4
Quinlan McNellen <riku@thisis.moe>
2016-03-02 14:43:51 -0500

    New class 'HelloWorld.java'

---
+++ ICS3U/src/mcnellen/HelloWorld.java
@@ -0,0 +1,15 @@
+package mcnellen;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		/* HelloWorld.java
+		 * This program prints Hello World! to the console
+		 * Quinlan McNellen
+		 * 2016/03/02
+		 */
+		System.out.println("Hello World!");
+		//I mean what did you think it would do?
+	}
+
+}
