fb55c3e112af2aa6f77770e875d0f443ee1f4034
root <root@(none)>
2016-03-08 14:47:52 -0500

    added comments

--- ICS3U/src/mcnellen/Circumference.java
+++ ICS3U/src/mcnellen/Circumference.java
@@ -3,11 +3,25 @@ package mcnellen;
 public class Circumference {
 
     public static void main(String[] args) {
+        /* Circumference.java
+         * This program calculates the circumference of a circle from the radius
+         * @author Quinlan McNellen
+         * 2016/03/08
+         */
+        
+        //Declare integer radius
         int radius;
+        //Declare integer circumference
         int circumference;
+        //Declare input scanner
+        Scanner scan = new Scanner(System.in);
+        //Print prompt for the radius value
         System.out.println("Radius: ");
-        radius = System.in.read();
+        //Read the value from the user
+        radius = scan.nextLine();
+        //Calculate circumference
         circumference = (3.14 * radius * 2);
+        //Print the output value
         System.out.print("Circumference: ");
         System.out.println(circumference);       
     }
