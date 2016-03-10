620069fb8c2219f38afc1ea9f556d4b098cf3d7c
Quinlan McNellen <riku@thisis.moe>
2016-03-10 14:39:52 -0500

    v0.0.1

---
+++ ICS3U/src/mcnellen/QuadraticFormula.java
@@ -0,0 +1,16 @@
+package mcnellen;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class QuadraticFormula {
+    
+    public static void main(String[] args) {
+        /* QuadraticFormula.java
+         * This program takes the values of a quadratic equation and solves for X 
+         * @author Quinlan McNellen
+         * 2016/03/10
+         * v0.0.0
+         */
+    }
+}
