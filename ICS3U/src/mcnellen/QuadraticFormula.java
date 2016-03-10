6e14a80fa39dd3c81afb168b19ed9c22fa5c17f4
Quinlan McNellen <riku@thisis.moe>
2016-03-10 14:41:45 -0500

    commenting

--- ICS3U/src/mcnellen/QuadraticFormula.java
+++ ICS3U/src/mcnellen/QuadraticFormula.java
@@ -10,7 +10,44 @@ public class QuadraticFormula {
          * This program takes the values of a quadratic equation and solves for X 
          * @author Quinlan McNellen
          * 2016/03/10
-         * v0.0.0
+         * v0.0.2
          */
+         //set version number
+         final String VERSION = "v0.0.2";
+         
+         //declare variables
+         double a;
+         double b;
+         double c;
+         double x1;
+         double x2;
+         double discriminant;
+         
+         //declare formatting scheme
+         DecimalFormat oneDecimal = new DecimalFormat ("$###,##0.00");
+         
+         //declare input scanner
+         Scanner scan = new Scannner(System.in);
+         
+         //output queries and parse input
+         System.out.println("QuadraticFormula " + VERSION);
+         System.out.print("Value of A: ");
+         a = Double.parseDouble(scan.nextLine());
+         System.out.print("Value of B: ");
+         b = Double.parseDouble(scan.nextLine());
+         System.out.print("Value of C: ");
+         c = Double.parseDouble(scan.nextLine());
+         
+         //close input scanner
+         scan.close();
+         
+         //calculate values
+         discriminant = Math.sqrt(Math.pow(b,2)-(4*a*c));
+         x1 = ((-1*b)+discriminant)/2;
+         x2 = ((-1*b)-discriminant)/2;
+         
+         //print output
+         System.out.println("X = " + oneDecimal.format(x1));
+         System.out.println("X = " + oneDecimal.format(x2));
     }
 }
