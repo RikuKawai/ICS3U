a599f4ff2f3fcb92381668222db459e7c3633257
Quinlan McNellen <riku@thisis.moe>
2016-03-23 14:36:51 -0400

    actual code

--- ICS3U/src/mcnellen/NameRater.java
+++ ICS3U/src/mcnellen/NameRater.java
@@ -8,6 +8,28 @@ public class NameRater {
         
         final String VERSION = "v0.0.1"
         
+        String name;
+        String ratingText;
+        int rating;
         
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("NameRater " + VERSION);
+        System.out.print("Enter your name: ");
+        name = scan.nextLine();
+        
+        scan.close();
+        
+        rating = (int)(Math.random() * 10) + 1;
+        
+        if (rating >= 1 && rating <= 3) {
+            ratingText = "I hate that";
+        } else if (rating >= 4 && rating <= 10) {
+            ratingText = "That's my favourite";
+        } else {
+            ratingText = "It's so bad it broke this program, that";
+        }
+        
+        System.out.println(name + "? " + ratingText + " name!");
     }
 }
