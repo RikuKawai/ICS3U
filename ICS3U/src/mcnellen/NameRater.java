cdc13a3600163ee841323daea692283f660d2280
Quinlan McNellen <riku@thisis.moe>
2016-03-23 14:20:13 -0400

    initial commit

---
+++ ICS3U/src/mcnellen/NameRater.java
@@ -0,0 +1,13 @@
+package mcnellen;
+
+import java.util.Scanner;
+
+public class NameRater {
+    
+    public static void main(String[] args) {
+        
+        final String VERSION = "v0.0.1"
+        
+        
+    }
+}
