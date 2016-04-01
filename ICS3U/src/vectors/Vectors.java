879adac2ba3a7c9cc398a2f76eaf4d20306b572e
Quinlan McNellen <riku@thisis.moe>
2016-04-01 16:41:55 -0400

    moved vectors to its own package

---
+++ ICS3U/src/vectors/Vectors.java
@@ -0,0 +1,22 @@
+package mcnellen;
+
+import java.util.Scanner;
+
+public class Vectors {
+    
+    public static void main(String[] args) {
+        double magnitudeX;
+        double magnitudeY;
+        double directionX;
+        double directionY;
+        
+        String poleFrom;
+        String poleTo;
+        double degreesChange;
+        
+        final String VERSION = "v0.0.1a";
+        
+        System.out.println("Vectors " + VERSION);
+        System.out.print()
+    }
+}
