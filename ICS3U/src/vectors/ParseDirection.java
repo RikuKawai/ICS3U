ee5f447fcbbe7fbb82b578e042e8557167d2fccf
Quinlan McNellen <riku@thisis.moe>
2016-04-01 16:53:04 -0400

    starting pole and absolute pole handling

--- ICS3U/src/vectors/ParseDirection.java
+++ ICS3U/src/vectors/ParseDirection.java
@@ -2,6 +2,31 @@ package vectors;
 
 public class ParseDirection {
     public static void main(String[] args) {
+        String poleFrom;
+        String poleTo;
+        double degreesChange;
+        double poleDegrees;
+        boolean subtract = false;
+        boolean noModifier = false;
         
+        if (poleFrom.equalsIgnoreCase("N")) {
+            poleDegrees = 0;
+        } else if (poleFrom.equalsIgnoreCase("E")) {
+            poleDegrees = 90;
+        } else if (poleFrom.equalsIgnoreCase("S")) {
+            poleDegrees = 180;
+        } else if (poleFrom.equalsIgnoreCase("W")) {
+            poleDegrees = 270;
+        }
+        
+        if (degreesChange == 0) {
+            noModifier = true;
+        } else {
+            noModifier = false;
+        }
+        
+        if (poleTo.equalsIgnoreCase("N")) {
+            
+        }
     }
 }
