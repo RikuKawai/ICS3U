eb2a6c7f3a1a8ecf8315bc253affc615ca607cb7
root <root@Megurine.home>
2016-04-24 19:23:49 -0400

    complete

--- ICS3U/src/mcnellen/NameList.java
+++ ICS3U/src/mcnellen/NameList.java
@@ -5,8 +5,33 @@ import java.util.Scanner;
 public class NameList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		final String VERSION = "v0.1";
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int numberOfNames;
+		
+		System.out.println("NameList " + VERSION);
+		System.out.print("Please specify the number of names: ");
+		numberOfNames = Integer.parseInt(scan.nextLine());
+		String[] name = new String[numberOfNames];
+		
+		System.out.println("Enter names");
+		for (int i = 0; i<numberOfNames; i++) {
+			System.out.print("Name " + (i+1) + ": ");
+			name[i] = scan.nextLine();
+		}
+		scan.close();
+		listNames(name);
 	}
-
+	public static void listNames(String[] a) {
+		int x = a.length - 1;
+		for (int i = 0; i<=x; i++) {
+			System.out.println(a[i]);
+		}
+		for (int i = x; i>=0; i--) {
+			System.out.println(a[i]);
+		}
+	}
 }
