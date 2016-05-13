e9fb442f44abc340c68e25f16b12383c0c18bb1b
McNellen <riku@thisis.moe>
2016-05-13 15:05:21 -0400

    tic tac toe game

---
+++ ICS3U/src/mcnellen/TicTacToe.java
@@ -0,0 +1,17 @@
+package mcnellen;
+
+//import hsa_new.*;
+import java.awt.*;
+
+public class TicTacToe {
+	
+	final static String VERSION = "v0.1";
+
+	public static void main(String[] args) {
+		
+		//Console c = new Console(30,100,11,"TicTacToe " + VERSION); //create console
+		Canvas c = new Canvas();
+		
+	}
+
+}
---
+++ ICS3U/src/mcnellen/TicTacToeWindow.java
@@ -0,0 +1,43 @@
+package mcnellen;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class TicTacToeWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TicTacToeWindow window = new TicTacToeWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TicTacToeWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
