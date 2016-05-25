450797f2ca6676ed0932701ae67f4c67dfb0dc02
McNellen <riku@thisis.moe>
2016-05-25 13:52:45 -0400

    fixed javadoc

--- ICS3U/src/mcnellen/FunWithMath.java
+++ ICS3U/src/mcnellen/FunWithMath.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public class FunWithMath {
 
 	public static void main(String[] args) throws IOException {
-		/*
+		/**
 		 * FunWithMath.java
 		 * This program accepts two integers from the user and then adds, subtracts, multiplies, and divides them
 		 * @author Quinlan McNellen
@@ -19,34 +19,34 @@ public class FunWithMath {
 		int num2 = Integer.parseInt(tokens[1]);
 		add(num1, num2); subtract(num1, num2); multiply(num1, num2); divide(num1, num2); //call methods
 	}
-	/*
+	/**
 	 * add two integers and print the sum
-	 * @param x int first number
-	 * @param y int second number
+	 * @param	x	first number
+	 * @param	y	second number
 	 */
 	public static void add(int x, int y) {
 		System.out.println(x+y);
 	}
-	/*
+	/**
 	 * subtract two integers and print the sum
-	 * @param x int first number
-	 * @param y int second number
+	 * @param	x	first number
+	 * @param	y	second number
 	 */
 	public static void subtract(int x, int y) { 
 		System.out.println(x-y);
 	}
-	/*
+	/**
 	 * multiply two integers and print the product
-	 * @param x int first number
-	 * @param y int second number
+	 * @param	x	first number
+	 * @param	y	second number
 	 */
 	public static void multiply(int x, int y) {
 		System.out.println(x*y);
 	}
-	/*
+	/**
 	 * divide two integers and print the product
-	 * @param x int first number
-	 * @param y int second number
+	 * @param	x	first number
+	 * @param	y	second number
 	 */
 	public static void divide(int x, int y) { 
 		System.out.println(x/y);
